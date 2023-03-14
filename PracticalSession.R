81e6e123424289da2135caaf00845ce47b973a96
Amal Damodar Mukund <amaldmukund@gmail.com>
2023-03-14 14:24:30 +0000

    initial commit

---
+++ PracticalSession.R
@@ -0,0 +1,31 @@
+x <- c(3, 12, 7)
+y <- c(4, 9, 11)
+z <- x + y
+z
+
+a <- x + 2
+a
+
+b <- y + 50
+b
+
+c <- sum(x)
+c
+
+d <- mean(y)
+d
+
+w <- c(2, 7, NA, 20)
+e <- sum(w)
+
+v <- sum(w, na.rm = TRUE)
+v
+
+typeof(x)
+
+x <- append(x, 12)
+x
+
+u <- c(5:12)
+u <- append (u, letters[6:10])
+u
